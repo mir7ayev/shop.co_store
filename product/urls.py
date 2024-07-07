fab757540a69120c5e0ebc31989639df9f037190
Samirxon <100518687+mir7ayev@users.noreply.github.com>
2024-07-07 23:40:11 +0500

    comment and search product urls added

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    ProductViewSet,
+    ProductViewSet, CommentViewSet,
 )
 
 urlpatterns = [
@@ -8,6 +8,11 @@ urlpatterns = [
     # Products
     path('products/', ProductViewSet.as_view({'get': 'list_products'})),
     path('product/<int:pk>/', ProductViewSet.as_view({'get': 'retrieve_product'})),
-    path('related/<int:pk>/', ProductViewSet.as_view({'get': 'related_products'})),
+    path('related_products/<int:pk>/', ProductViewSet.as_view({'get': 'related_products'})),
+    path('products/search/', ProductViewSet.as_view({'get': 'search_products'})),
+
+    # Comments
+    path('comments/', CommentViewSet.as_view({'get': 'list_comments'})),
+    path('comment/', CommentViewSet.as_view({'post': 'create_comment'})),
 
 ]
