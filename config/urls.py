88b0f1be2e1a6227f9b0176c6f175ca3f7965a42
Samirxon <100518687+mir7ayev@users.noreply.github.com>
2024-07-07 23:40:31 +0500

    product url is modified

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/api/v1/product/', include('product.urls')),
+    path('/api/v1/', include('product.urls')),
     # path('/api/v1/cart/', include('cart.urls')),
     # path('/api/v1/order/', include('order.urls')),
     # path('/api/v1/core/', include('core.urls')),
