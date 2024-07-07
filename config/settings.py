4d726c1b6592fd3186195b0a73487cb7a8818cdb
Samirxon <mirzayevsamirxon@gmail.com>
2024-07-07 14:38:10 +0500

    Ckeditor added to installed apps

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
     # System packages
     'rest_framework',
+    'ckeditor',
 
     # Local apps
     'core',
@@ -113,3 +114,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
+}
