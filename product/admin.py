0b9625f2fd4f04e9403b75e304505d38b0530826
Samirxon <mirzayevsamirxon@gmail.com>
2024-07-07 14:39:55 +0500

    Bismillah

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import (
+    ProductCategory, ProductColor, ProductSize, Product,
+    ProductComment,
+)
 
-# Register your models here.
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+
+@admin.register(ProductColor)
+class ProductColorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+
+@admin.register(ProductSize)
+class ProductSizeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'gender', 'created_at', 'is_available')
+    list_display_links = ('id', 'name')
+
+
+@admin.register(ProductComment)
+class ProductCommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'product', 'created_at', 'is_active')
+    list_display_links = ('id', 'author')
