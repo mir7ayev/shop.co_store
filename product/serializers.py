60f056297e80639f3ac6727c3b2ab8e2d76e951e
Samirxon <100518687+mir7ayev@users.noreply.github.com>
2024-07-08 14:22:17 +0500

    color, size serializers added and product to_representation is optimized

--- product/serializers.py
+++ product/serializers.py
@@ -2,7 +2,7 @@ from rest_framework.serializers import ModelSerializer
 from django.contrib.auth import get_user_model
 from django.http import Http404
 from .models import (
-    Product, ProductComment,
+    Product, ProductComment, ProductColor, ProductSize,
 )
 
 User = get_user_model()
@@ -10,15 +10,27 @@ User = get_user_model()
 
 # TODO: get user from other micro-service
 
+class ProductColorSerializer(ModelSerializer):
+    class Meta:
+        model = ProductColor
+        fields = '__all__'
+
+
+class ProductSizeSerializer(ModelSerializer):
+    class Meta:
+        model = ProductSize
+        fields = '__all__'
+
+
 class ProductCommentSerializer(ModelSerializer):
     class Meta:
         model = ProductComment
         fields = '__all__'
 
     def save(self, **kwargs):
-        request = self.context['request']
-        user = User.objects.filter(user_id=request.user.id).first()
+        request = self.context.get('request', None)
 
+        user = User.objects.filter(user_id=request.user.id).first()
         if user is None:
             raise Http404('User not found')
 
@@ -30,10 +42,16 @@ class ProductCommentSerializer(ModelSerializer):
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('id', 'created_at', 'updated_at', 'name', 'category', 'gender',
+                  'image', 'description', 'price', 'discount', 'price_with_discount')
 
     def to_representation(self, instance):
+        many = self.context.get('many', None)
         representation = super().to_representation(instance)
 
-        representation['comments'] = ProductCommentSerializer(ProductComment.objects.filter(movie_id=instance.id),
-                                                              many=True).data
+        if many is False:
+            representation['colors'] = ProductColorSerializer(instance.colors.all(), many=True).data
+            representation['sizes'] = ProductSizeSerializer(instance.sizes.all(), many=True).data
+            representation['comments'] = ProductCommentSerializer(instance.productcomment_set.all(), many=True).data
+
+        return representation
