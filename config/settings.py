01f8bcc04243886fb5c135f5f45741fcd322b98a
Samirxon <100518687+mir7ayev@users.noreply.github.com>
2024-07-08 14:20:53 +0500

    static and media settings are added

--- config/settings.py
+++ config/settings.py
@@ -109,6 +109,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
