5375e630faea632e2f3bc801be96d6d57933273c
Samirxon <100518687+mir7ayev@users.noreply.github.com>
2024-07-11 19:26:49 +0500

    Product category serializer added and other some file modified

--- product/serializers.py
+++ product/serializers.py
@@ -1,41 +1,46 @@
 from rest_framework.serializers import ModelSerializer
-from django.contrib.auth import get_user_model
 from django.http import Http404
 from .models import (
-    Product, ProductComment, ProductColor, ProductSize,
+    Product, ProductReview, ProductColor, ProductSize,
+    ProductCategory, ProductImage,
 )
 
-User = get_user_model()
 
+class ProductCategorySerializer(ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = ('id', 'name')
 
-# TODO: get user from other micro-service
 
 class ProductColorSerializer(ModelSerializer):
     class Meta:
         model = ProductColor
-        fields = '__all__'
+        fields = ('id', 'name')
 
 
 class ProductSizeSerializer(ModelSerializer):
     class Meta:
         model = ProductSize
-        fields = '__all__'
+        fields = ('id', 'name')
 
 
-class ProductCommentSerializer(ModelSerializer):
+class ProductImageSerializer(ModelSerializer):
     class Meta:
-        model = ProductComment
-        fields = '__all__'
+        model = ProductImage
+        fields = ('id', 'image', 'alt_text')
+
+
+class ProductReviewSerializer(ModelSerializer):
+    class Meta:
+        model = ProductReview
+        fields = ('id', 'product', 'author', 'rating', 'comment', 'created_at')
 
     def save(self, **kwargs):
-        request = self.context.get('request', None)
-
-        user = User.objects.filter(user_id=request.user.id).first()
-        if user is None:
+        user_id = self.context.get('user_id', None)
+        if user_id is None:
             raise Http404('User not found')
 
-        self.validated_data['author'] = user
-
+        self.validated_data['author'] = user_id
         return super().save(**kwargs)
 
 
@@ -43,15 +48,16 @@ class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'created_at', 'updated_at', 'name', 'category', 'gender',
-                  'image', 'description', 'price', 'discount', 'price_with_discount')
+                  'images', 'description', 'price', 'discount', 'price_with_discount')
 
     def to_representation(self, instance):
         many = self.context.get('many', None)
         representation = super().to_representation(instance)
+        representation['images'] = ProductImageSerializer(instance.images.all(), many=True).data
 
         if many is False:
             representation['colors'] = ProductColorSerializer(instance.colors.all(), many=True).data
             representation['sizes'] = ProductSizeSerializer(instance.sizes.all(), many=True).data
-            representation['comments'] = ProductCommentSerializer(instance.productcomment_set.all(), many=True).data
+            representation['comments'] = ProductReviewSerializer(instance.productreview_set.all(), many=True).data
 
         return representation
