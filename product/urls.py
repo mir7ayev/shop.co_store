253116f3e0d539e66ec6d99e96fad60324054eaf
Samirxon <100518687+mir7ayev@users.noreply.github.com>
2024-07-11 19:24:51 +0500

    some urls combined and optimized

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    ProductViewSet, CommentViewSet,
+    ProductViewSet, ReviewViewSet,
 )
 
 urlpatterns = [
@@ -8,11 +8,9 @@ urlpatterns = [
     # Products
     path('products/', ProductViewSet.as_view({'get': 'list_products'})),
     path('product/<int:pk>/', ProductViewSet.as_view({'get': 'retrieve_product'})),
-    path('related_products/<int:pk>/', ProductViewSet.as_view({'get': 'related_products'})),
-    path('products/search/', ProductViewSet.as_view({'get': 'search_products'})),
 
-    # Comments
-    path('comments/', CommentViewSet.as_view({'get': 'list_comments'})),
-    path('comment/', CommentViewSet.as_view({'post': 'create_comment'})),
+    # Reviews
+    path('reviews/', ReviewViewSet.as_view({'get': 'list_reviews'})),
+    path('review/', ReviewViewSet.as_view({'post': 'create_review'})),
 
 ]
