61399c5c99a6712e4c816783fac1dc4553d5b04e
Samirxon <100518687+mir7ayev@users.noreply.github.com>
2024-07-27 19:59:27 +0500

    raise_for_status() function added

--- core/utils.py
+++ core/utils.py
@@ -8,8 +8,7 @@ def check_service_access_token(service_access_token):
         url="http://134.122.76.27:8026/api/v1/check/token/",
         data={'secret_token': service_access_token}
     )
-    if response.status_code != 200:
-        return response.status_code
+    response.raise_for_status()
 
     return True
 
@@ -19,20 +18,18 @@ def get_service_access_token():
         url="http://134.122.76.27:8026/api/v1/create/token/",
         data={'secret_service_key': settings.AUTH_SECRET_KEY}
     )
-    if response.status_code != 201:
-        return response.status_code
+    response.raise_for_status()
 
     return response.json().get('secret_token')
 
 
 def get_user_data(user_access_token):
     service_access_token = get_service_access_token()
-    response = requests.get(
+    response = requests.post(
         url="http://134.122.76.27:8025/api/v1/auth/single/user/",
         headers={'Authorization': f'Bearer {user_access_token}'},
-        data={'user_access_token': service_access_token}
+        data={'secret_token': service_access_token}
     )
-    if response.status_code != 200:
-        return response.status_code
+    response.raise_for_status()
 
     return response.json()
